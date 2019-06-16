00eb073ca4d8fc74db415ebe5054cd6cc464a3d3
c3mohamn <nasir.m23@hotmail.com>
2019-06-16 00:31:12 -0400

    Add about website

--- app-nm/src/app/about/about.module.ts
+++ app-nm/src/app/about/about.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about/about.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AboutComponent],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    AngularSvgIconModule,
+    HttpClientModule
   ]
 })
 export class AboutModule { }
