00d04a3b7e1d314f0d13b151b2c6c4cbe9b462bd
c3mohamn <nasir.m23@hotmail.com>
2019-06-16 21:00:27 -0400

    Update about page

---
+++ app-nm/src/app/shared/data/about-icon-list.ts
@@ -0,0 +1,41 @@
+export const iconList: any[] = [
+  {
+    title: 'Angular', class: 'angular'
+  },
+  {
+    title: 'Node', class: 'node'
+  },
+  {
+    title: 'Typescript', class: 'typescript'
+  },
+  {
+    title: 'Javascript', class: 'javascript'
+  },
+  {
+    title: 'Sass', class: 'sass'
+  },
+  {
+    title: 'Material Design', class: 'material-design'
+  },
+  {
+    title: 'Github', class: 'github'
+  },
+  {
+    title: 'Heroku', class: 'heroku'
+  },
+  {
+    title: 'Trello', class: 'trello'
+  },
+  {
+    title: 'Google Analytics', class: 'google-analytics'
+  },
+  {
+    title: 'Google Tag Manager', class: 'google-tag-manager'
+  },
+  {
+    title: 'Photoshop', class: 'photoshop'
+  },
+  {
+    title: 'Travis', class: 'travis'
+  }
+];
