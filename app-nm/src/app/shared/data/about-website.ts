2d528644f66255e4a9f63c5b45bdc62780b50548
Nasir Mohammad <nasir.m23@hotmail.com>
2019-06-18 22:23:23 -0400

    Add about me to About page

--- app-nm/src/app/shared/data/about-website.ts
+++ app-nm/src/app/shared/data/about-website.ts
@@ -1,85 +1,126 @@
-import { AboutIcon } from '../models/about-icon.model.ts';
+import { TechIcon } from '../models/about-tech.model';
 
-export const iconList: AboutIcon[] = [
+export const howItStarted = `
+      After finishing my undergraduate degree and beginning my job hunt, I came to the realization
+      that like artists, web developers can also have portfolios.
+      <br /><br />
+      Even though I might be lacking in content - as my journey has only begun - I figured I should
+      start building my own portfolio, adding on to it as I developed more skills and experiences.
+      It could be a place for me to easily showcase who I am and what I apsire to be. A place where
+      I could keep a log of everything I learn. Hopefully it captures, to some extent, who I am and
+      what I have to offer.
+`;
+
+export const iconList: TechIcon[] = [
   {
-    title: 'Visual Studio Code', class: 'vscode',
+    title: 'Visual Studio Code',
+    class: 'vscode',
     description: `Visual Studio Code is a code editor developed by Microsoft. It contains a
      plethora of useful extensions, is lightweight, and free!`
   },
   {
-    title: 'Angular', class: 'angular',
+    title: 'Angular',
+    class: 'angular',
     description: `Angular is a web and mobile application framework
      led by the Angular Team at Google. It has many tools built in like it's own testing
      framework, and tons of support and resources.`
   },
   {
-    title: 'Nodejs', class: 'node', description: `Nodejs is an open-source, cross-platform
+    title: 'Nodejs',
+    class: 'node',
+    description: `Nodejs is an open-source, cross-platform
      runtime environment for developing server-side and networking applications. I use it
      because it is incredibly easy to use and set up.`
   },
   {
-    title: 'Foundation', class: 'foundation', description: `Foundation is a front-end framework
+    title: 'Foundation',
+    class: 'foundation',
+    description: `Foundation is a front-end framework
      like Bootstrap. It is very customizable and allows you to include the features you want. I
      primarily use it for it's scss functions and grid system.`
   },
   {
-    title: 'Material Design', class: 'material-design', description: `Material is a design concept
+    title: 'Material Design',
+    class: 'material-design',
+    description: `Material is a design concept
      created by Google. I have used, on several occasions, Angular Material components, icons and fonts.
      For this website, I have only used their fonts and icons thus far.`
   },
   {
-    title: 'Github', class: 'github', description: `Github is a hosting service for version control using
+    title: 'Github',
+    class: 'github',
+    description: `Github is a hosting service for version control using
      Git. It's free, has tons of integrations, and is very easy to navigate and use.`
   },
   {
-    title: 'Heroku', class: 'heroku', description: `Heroku is a cloud platform as a service (PaaS)
+    title: 'Heroku',
+    class: 'heroku',
+    description: `Heroku is a cloud platform as a service (PaaS)
      that allows developers to build, run, and operate applications entirely in the cloud. I have been
      using Heroku for a coupple years now as they provide a free plan that hosts your apps. You can also
      add a custom domain for free, which is what I have done for this website.`
   },
   {
-    title: 'Trello', class: 'trello', description: `Trello is a web-based list-making application.
+    title: 'Trello',
+    class: 'trello',
+    description: `Trello is a web-based list-making application.
      Currently I use it as a kanban board to keep track of ideas and tasks. It is extremely easy to use
      and also provides tons of integrations.`
   },
   {
-    title: 'Google Analytics', class: 'google-analytics', description: `Google Analytics is a web analytics
+    title: 'Google Analytics',
+    class: 'google-analytics',
+    description: `Google Analytics is a web analytics
      service offered by Google that allows you to track website traffic. I use it to check if anyone even looks
      at this website - and it's free!`
   },
   {
-    title: 'Google Tag Manager', class: 'google-tag-manager', description: `Google Tag Manager is a tag
+    title: 'Google Tag Manager',
+    class: 'google-tag-manager',
+    description: `Google Tag Manager is a tag
      management system used for tracking and analytics of websites. It pairs well with Google Analytics
      and I use to track page views and clicks.`
   },
   {
-    title: 'Photoshop', class: 'photoshop', description: `A graphic editor developed by Adobe. I use on it
+    title: 'Photoshop',
+    class: 'photoshop',
+    description: `A graphic editor developed by Adobe. I use on it
      occasion to modify svgs and images that I use for my apps. Photoshop and graphic design in
      general is an area that I really would like to improve on.`
   },
   {
-    title: 'Travis CI', class: 'travis', description: `Travis is a continuous integration service
+    title: 'Travis CI',
+    class: 'travis',
+    description: `Travis is a continuous integration service
      used to build and test projects hosted on Github. I currently use it to run Angular tests when code
      is pushed to the master branch.`
   },
   {
-    title: 'HTML', class: 'html', description: `Hypertext Markup Language is the standard markup language for
+    title: 'HTML',
+    class: 'html',
+    description: `Hypertext Markup Language is the standard markup language for
      documents to be displayed on a web browser. I did not even want to put it in this list, but I have since it is
      listed in many job descriptions I have read.`
   },
   {
-    title: 'TypeScript', class: 'typescript', description: `Typescript is a superset of JavaScript. Angular
+    title: 'TypeScript',
+    class: 'typescript',
+    description: `Typescript is a superset of JavaScript. Angular
      has it built-in and it can also used with Nodejs (which I do for this website). It provides types, linting,
      and error checking that makes it so much better to work with than JavaScript.`
   },
   {
-    title: 'JavaScript', class: 'javascript', description: `JavaScript is not quite Java. It is a high-level,
+    title: 'JavaScript',
+    class: 'javascript',
+    description: `JavaScript is not quite Java. It is a high-level,
      light-weight, interpreted programming language. It is primarily known as the scripting language
      for web apps. I actually have not written any JavaScript for this website, as I use TypeScript for both.
      But since TypeScript is a superset of JavaScript, I guess I have.`
   },
   {
-    title: 'Sass', class: 'sass', description: `Sass is probably the most popular CSS extension language.
+    title: 'Sass',
+    class: 'sass',
+    description: `Sass is probably the most popular CSS extension language.
      It has many capabilities and features such as built-in functions, variables, rules, operations,
      interpolation and much more.`
   }
