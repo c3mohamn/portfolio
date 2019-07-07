2e364db079c2c0786a015b1f654515cbe4c6c27a
c3mohamn <nasir.m23@hotmail.com>
2019-07-07 05:23:58 -0400

    Add admin secret and password to config

--- server/config/config.ts
+++ server/config/config.ts
@@ -8,6 +8,8 @@ function getConfigData(): Config {
   } else {
     config.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
     config.MONGODB_URI = process.env.MONGODB_URI;
+    config.ADMIN_SECRET = process.env.ADMIN_SECRET;
+    config.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
   }
 
   return config;
