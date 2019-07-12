69a00011093dc741ccb6cba2658c659a0437140e
c3mohamn <nasir.m23@hotmail.com>
2019-07-12 04:24:39 -0400

    Add admin controller to server

---
+++ server/controllers/admin.controller.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import Joi from 'joi';
+import config from '../config/config';
+
+const router = express.Router();
+
+router.get('', (req: Request, res: Response) => {
+  console.log('get /admin');
+  res.status(200).send({ message: 'Get works!' });
+});
+
+router.post('/verify', (req: Request, res: Response) => {
+  // console.log('post /admin/verify');
+  const password: string = req.body.password;
+
+  if (password === config.ADMIN_PASSWORD) {
+    // TODO: Change to JWT token
+    res.status(200).send({ token: 'temp' });
+  } else {
+    res.status(400).send({ message: 'Incorrect password.' });
+  }
+
+  const adminPassword = config.ADMIN_PASSWORD;
+});
+
+export default router;
--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import contact from './contact.controller';
 import about from './about.controller';
+import admin from './admin.controller';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from '../swagger.json';
 
@@ -8,6 +9,7 @@ const router = express.Router();
 
 router.use('/about', about);
 router.use('/contact', contact);
+router.use('/admin', admin);
 
 // setup swagger
 router.use('/swagger', swaggerUi.serve);
