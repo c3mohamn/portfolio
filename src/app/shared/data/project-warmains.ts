f26cf292d01bd2f5bed22c3ed8c6af74a6aad4eb
Nasir Mohammad <nasir.m23@hotmail.com>
2019-07-19 00:31:38 -0400

    Start projects: warmains, telegram, admin panel

---
+++ src/app/shared/data/project-warmains.ts
@@ -0,0 +1,71 @@
+import { Project, ProjectType } from '../models/project.model';
+
+export const warmainsProject: Project = {
+  name: 'warmains',
+  title: `Warmains`,
+  shortDescription: `A character, talent, and raid planning tool for World of Warcraft.`,
+  type: ProjectType.Personal,
+  tags: ['Angular', 'Nodejs', 'Mongo', 'Gaming', 'Personal'],
+  svgLogoName: 'horde-logo',
+  previewImgRelativePath: 'assets/images/projects/preview/warmains.png',
+  overview: `World of Warcraft is one of the largest MMORPG games of all time. At it's peak it had over 10 million active
+  players. The game is also known for it's huge learning curve and the community has built thousands of online tools and
+  in-game addons to aid players.
+  <br><br>
+  Warmains is one of these tools. It is a character planning tool that allows players to play around
+  with different gear combinations to optimize their character's statistics.
+  <br><br>
+  <b>Why not just do this in the game?</b><br><br>
+  <div class="grid-x grid-margin-x grid-margin-y">
+    <div class="cell small-12 medium-4">
+    Well, <u>gear is hard to get</u>. Especially the gear people want. You have to organize 10-40 man raids, slay dragons or demi-gods
+  and one small mistake from one of those 40 players can cause your entire raid to die.
+  It would be better to know what gear to aim for before you go off into the game and spend hours, even days, trying to obtain it.
+    </div>
+    <div class="cell small-12 medium-8">
+    <a href="assets/images/projects/warmains/in-game-wipe.jpg" target="_blank" rel="noopener" title="Lich King Wipe"
+  aria-label="Lich King Wipe Image">
+  <img src="assets/images/projects/warmains/in-game-wipe.jpg" alt="Warcraft Wiping on Boss Encounter">
+  </a>
+    </div>
+  </div>
+
+  <br><br>
+  This is where Warmains comes in. In addition to testing out gear, Warmains allows you to also test out talent points.
+  <br><br>
+  <b>What are talent points?</b><br><br>
+  <div class="grid-x grid-margin-x grid-margin-y align-center">
+    <div class="cell small-12 medium-6">
+    <a href="assets/images/projects/warmains/priest-talents.jpg" target="_blank" rel="noopener" title="Priest Talents"
+  aria-label="Priest Talents Image">
+  <img src="assets/images/projects/warmains/priest-talents.jpg" alt="Priest Talents Image">
+  </a>
+
+    </div>
+    <div class="cell small-12 medium-6">
+    Talent points are points that your character gains each level from level 10 onwards. They can boost your stats, improve your current
+  spells or even give you new spells. But you have a limited amount, and must choose wisely where you spend it. You can reset your talents
+  and try out new builds, <u>but it costs gold</u>.
+    </div>
+    <div class="cell small-12 medium-6">
+    <a href="assets/images/projects/warmains/in-game-reset-small.png" target="_blank" rel="noopener" title="Talent Reset"
+    aria-label="Talent Reset Image">
+    <img src="assets/images/projects/warmains/in-game-reset-small.png" alt="Warcraft Talent Reset Gold Cost">
+    </a>
+    </div>
+  </div>
+  <br><br>
+  Again, this is where Warmains comes in allowing you to test out talent points before you spend them without having to spend any gold
+  in-game.`,
+  // goals: `
+  // <ul>
+  //   <li>Create a character planner that allows players to </li>
+  //   <li>Re-create similar interfaces for character statistics and character talents panel.</li>
+  //   <li>Re-create logic (formulas, algorithms, rules) for character statistics and talents.</li>
+  //   <li></li>
+  // </ul>
+  // `,
+  contributions: ``,
+  process: ``,
+  outcome: ``
+};
--- src/app/shared/data/projects.ts
+++ src/app/shared/data/projects.ts
@@ -1,78 +1,56 @@
-import { Project, ProjectType } from '../models/project.model';
+import { Project } from '../models/project.model';
+import { warmainsProject } from './project-warmains';
+import { telegramBotProject } from './project-telegram-bot';
+import { adminPanelProject } from './project-admin-panel';
 
 export const projects: Project[] = [
-  {
-    name: 'source-admin-panel',
-    title: `Source Waitlist Admin Panel`,
-    shortDescription: `An admin panel allowing the marketing team easily perform CRUD commands for users, email templates, and more.`,
-    type: ProjectType.Work,
-    tags: ['Angular', '.NET', 'SQL', 'Professional'],
-    svgLogoName: 'source-logo',
-    previewImgRelativePath: 'assets/images/projects/preview/source.png'
-  },
-  {
-    name: 'source-telegram-bot',
-    title: `Source Telegram Bot`,
-    shortDescription: `An interactive Telegram bot promoting social engagement.`,
-    type: ProjectType.Work,
-    tags: ['Telegram', '.NET', 'API', 'Professional'],
-    svgLogoName: 'telegram',
-    previewImgRelativePath: 'assets/images/projects/preview/telegram-bot.png',
-    overviewImgRelativePath: 'assets/images/projects/preview/telegram-bot.png'
-  },
-  {
-    name: 'warmains',
-    title: `Warmains`,
-    shortDescription: `A character, talent, and raid planning tool for World of Warcraft.`,
-    type: ProjectType.Personal,
-    tags: ['Angular', 'Nodejs', 'Mongo', 'Gaming', 'Personal'],
-    svgLogoName: 'horde-logo',
-    previewImgRelativePath: 'assets/images/projects/preview/warmains.png'
-  },
-  {
-    name: 'book-me',
-    title: `Book-Me`,
-    shortDescription: `An office hour scheduler for students, TAs, and instructors.`,
-    type: ProjectType.School,
-    tags: ['Angular', 'Nodejs', 'Mongo', 'Engineering', 'School'],
-    svgLogoName: 'book-me-logo',
-    previewImgRelativePath: 'assets/images/projects/preview/book-me.png'
-  },
-  {
-    name: 'course-correct',
-    title: `Course Correct`,
-    shortDescription: `A concatenation of numerous course selection helpers in one app.`,
-    type: ProjectType.School,
-    tags: ['Android', 'Java', 'Engineering', 'School'],
-    svgLogoName: 'course-correct-logo',
-    previewImgRelativePath: 'assets/images/projects/preview/course-correct.png'
-  },
-  {
-    name: 'hands-for-mental-health',
-    title: `Hands for Mental Health`,
-    shortDescription: `Prototyping and designing solutions for mental health awareness.`,
-    type: ProjectType.School,
-    tags: ['Axure', 'HCI', 'Prototyping', 'Research', 'School'],
-    svgLogoName: 'mental-health',
-    previewImgRelativePath:
-      'assets/images/projects/preview/hands-for-mental-health.png'
-  },
-  {
-    name: 'cineplex',
-    title: `Cineplex Heuristic Evaluation & Usability Study`,
-    shortDescription: `Qualitative evaluation of Cineplex's web interface.`,
-    type: ProjectType.School,
-    tags: ['Heuristics', 'HCI', 'Heuristics', 'Study', 'School'],
-    svgLogoName: 'cineplex',
-    previewImgRelativePath: 'assets/images/projects/preview/cineplex.png'
-  },
-  {
-    name: 'smart-watch-keyboard',
-    title: `Smart Watch Keyboard Controlled Experiment`,
-    shortDescription: `Quantitative evaluation of miniature keyboard techniques for smart watches.`,
-    type: ProjectType.School,
-    tags: ['React', 'HCI', 'Experiment', 'Study', 'School'],
-    svgLogoName: 'keyboard',
-    previewImgRelativePath: 'assets/images/projects/preview/smart-watch.png'
-  }
+  warmainsProject,
+  adminPanelProject,
+  telegramBotProject
+  // {
+  //   name: 'book-me',
+  //   title: `Book-Me`,
+  //   shortDescription: `An office hour scheduler for students, TAs, and instructors.`,
+  //   type: ProjectType.School,
+  //   tags: ['Angular', 'Nodejs', 'Mongo', 'Engineering', 'School'],
+  //   svgLogoName: 'book-me-logo',
+  //   previewImgRelativePath: 'assets/images/projects/preview/book-me.png'
+  // },
+  // {
+  //   name: 'course-correct',
+  //   title: `Course Correct`,
+  //   shortDescription: `A concatenation of numerous course selection helpers in one app.`,
+  //   type: ProjectType.School,
+  //   tags: ['Android', 'Java', 'Engineering', 'School'],
+  //   svgLogoName: 'course-correct-logo',
+  //   previewImgRelativePath: 'assets/images/projects/preview/course-correct.png'
+  // },
+  // {
+  //   name: 'hands-for-mental-health',
+  //   title: `Hands for Mental Health`,
+  //   shortDescription: `Prototyping and designing solutions for mental health awareness.`,
+  //   type: ProjectType.School,
+  //   tags: ['Axure', 'HCI', 'Prototyping', 'Research', 'School'],
+  //   svgLogoName: 'mental-health',
+  //   previewImgRelativePath:
+  //     'assets/images/projects/preview/hands-for-mental-health.png'
+  // },
+  // {
+  //   name: 'cineplex',
+  //   title: `Cineplex Heuristic Evaluation & Usability Study`,
+  //   shortDescription: `Qualitative evaluation of Cineplex's web interface.`,
+  //   type: ProjectType.School,
+  //   tags: ['Heuristics', 'HCI', 'Heuristics', 'Study', 'School'],
+  //   svgLogoName: 'cineplex',
+  //   previewImgRelativePath: 'assets/images/projects/preview/cineplex.png'
+  // },
+  // {
+  //   name: 'smart-watch-keyboard',
+  //   title: `Smart Watch Keyboard Controlled Experiment`,
+  //   shortDescription: `Quantitative evaluation of miniature keyboard techniques for smart watches.`,
+  //   type: ProjectType.School,
+  //   tags: ['React', 'HCI', 'Experiment', 'Study', 'School'],
+  //   svgLogoName: 'keyboard',
+  //   previewImgRelativePath: 'assets/images/projects/preview/smart-watch.png'
+  // }
 ];
